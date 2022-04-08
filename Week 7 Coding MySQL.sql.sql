4798c086bc5167c45797b2692b7f57cd2af16182
Jessica  Smiejan <99990744+JessicaSmiejan@users.noreply.github.com>
2022-04-07 18:02:55 -0700

    Update Week 7 Coding MySQL.sql.sql

--- Week 7 Coding MySQL.sql.sql
+++ Week 7 Coding MySQL.sql.sql
@@ -1,1 +1,10 @@
+select * from employees where birth_date = ' 1965-01-01';
+select * from  employees where gender = 'F' and  hire_date >= '1990-01-01';
+Select first_name, last_name from employees where last_name like 'F%' limit 50;
+insert into employees values ( 100, '2004-04-04', 'April', 'Flowers', 'F', '2022-04-01');
+insert into employees values ( 101, '2005-05-05', 'Mark', 'May', 'M', '2021-05-01');
+insert into employees values ( 102, '2006-06-06', 'June', 'Birdie', 'F', '2020-06-15');
+Update employees Set first_name = 'Bob'  where emp_no =10023;
+update employees set  hire_date = '2002-01-01' where first_name like 'P%' or  last_name like 'P%'; 
+delete from  employees where  emp < 10000; 
 delete from  employees where emp_no In ('10048', '10099', '10234', '20089'); 
